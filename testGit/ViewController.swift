832aa61d6441c7371592df6fb345208859c54bed
TaigaKuroiwa <taigakuroiwa@taylors-ipod.lcsc.edu>
2016-09-29 12:15:01 -0700

    Initial Commit

---
+++ testGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by Taiga Kuroiwa on 2016/09/29.
+//  Copyright © 2016年 Taiga Kuroiwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
